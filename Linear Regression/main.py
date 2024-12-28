8831230afe1b513360a61ea2590301d7b6a7bc13
Kirill_Fridochkin <kfridochkin@mail.ru>
2024-12-28 17:28:23 +0300

    Added fit and get_coeffs function. Used gradient learning

--- Linear Regression/main.py
+++ Linear Regression/main.py
@@ -1,8 +1,11 @@
 import pandas as pd
+import numpy as np
+from functools import reduce
+from operator import add
 
 
 class MyLineReg:
-    def __init__(self, weight, n_iter=100, learning_rate=0.1):
+    def __init__(self, weights=[], n_iter=100, learning_rate=0.1):
         '''
         n_iter - количество шагов градиентного спуска
         learning_rate - коэффициент скорости обучения градиентного спуска
@@ -10,15 +13,33 @@ class MyLineReg:
         '''
         self.n_iter = n_iter
         self.learning_rate = learning_rate
+        self.weights = weights
 
-    def fit(self, X, y, verbose=False):
+    def fit(self, X, y_true, verbose=False):
         '''X - все входные признаки (pd.DataFrame)
            y - таргет (pd.Series),
            verbose - на какой итерации выводить лог'''
+        X.insert(0, 'col_ones', 1)
+        n = X.shape[1]
+        X = X.to_numpy()
+        self.weights = [1 for _ in range(n)]
+        for i in range(self.n_iter):
+            y_pred = np.dot(X, self.weights)
+            residuals = list(map(lambda x, y: x - y, y_pred, y_true))
+            loss = (1 / X.shape[0] *
+                    reduce(add, list(map(lambda x, y: (x - y) ** 2, y_pred, y_true))))
+            grad_mse = 2 / X.shape[0] * np.dot(residuals, X)               # градиент MSE
+            self.weights = self.weights - self.learning_rate * grad_mse    # шаг в сторону антиградиента
 
+            # вывод лога обучения модели
+            if verbose:
+                if i == 0:
+                    print(f'start | loss: {loss}')
+                elif (i + 1) % verbose == 0:
+                    print(f'{(i + 1) // verbose * verbose} | loss: {loss}')
+
+    def get_coef(self):
+        return self.weights[1:]
 
     def __str__(self):
         return f'MyLineReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}'
-
-a = MyLineReg()
-print(a)
--- Linear Regression/testing.py
+++ Linear Regression/testing.py
@@ -1,2 +1,20 @@
+from sklearn.datasets import make_regression
+import pandas as pd
+import numpy as np
+from main import MyLineReg
+
+X, y = make_regression(n_samples=1000,
+                       n_features=5,
+                       n_informative=2,
+                       noise=15,
+                       random_state=42)
+X = pd.DataFrame(X)
+y = pd.Series(y)
+X.columns = [f'col_{col}' for col in X.columns]
+
+a = MyLineReg()
+a.fit(X, y, verbose=False)
+print(a.get_coef())
+
 
 
