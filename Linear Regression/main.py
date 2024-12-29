4d5c9cd0e396830c474180976526d401e40ceb7d
Kirill_Fridochkin <kfridochkin@mail.ru>
2024-12-29 14:29:28 +0300

    Added correct docstrings and predicting functions

--- Linear Regression/main.py
+++ Linear Regression/main.py
@@ -6,29 +6,37 @@ from operator import add
 
 class MyLineReg:
     def __init__(self, weights=[], n_iter=100, learning_rate=0.1):
-        '''
-        n_iter - количество шагов градиентного спуска
-        learning_rate - коэффициент скорости обучения градиентного спуска
-        weights - веса модели
-        '''
+        """
+        Функция инициализирует объект класса
+
+        :param weights: Изначальные веса модели
+        :param n_iter: Количество циклов градиентного спуска
+        :param learning_rate: Шаг градиентного спуска
+        """
         self.n_iter = n_iter
         self.learning_rate = learning_rate
         self.weights = weights
 
-    def fit(self, X, y_true, verbose=False):
-        '''X - все входные признаки (pd.DataFrame)
-           y - таргет (pd.Series),
-           verbose - на какой итерации выводить лог'''
-        X.insert(0, 'col_ones', 1)
-        n = X.shape[1]
-        X = X.to_numpy()
+    def fit(self, x_train, y_train, verbose=False):
+        """
+        Функция обучает модель, функция потерь MSE, минимум функции
+        определяется градиентным спуском
+
+        :param x_train: Входные признаки
+        :param y_train: Целевой признак
+        :param verbose: Вывод лога обучения
+        :return: None
+        """
+        x_train.insert(0, 'col_ones', 1)
+        n = x_train.shape[1]
+        x_train = x_train.to_numpy()
         self.weights = [1 for _ in range(n)]
         for i in range(self.n_iter):
-            y_pred = np.dot(X, self.weights)
-            residuals = list(map(lambda x, y: x - y, y_pred, y_true))
-            loss = (1 / X.shape[0] *
-                    reduce(add, list(map(lambda x, y: (x - y) ** 2, y_pred, y_true))))
-            grad_mse = 2 / X.shape[0] * np.dot(residuals, X)               # градиент MSE
+            y_pred = np.dot(x_train, self.weights)
+            residuals = list(map(lambda x, y: x - y, y_pred, y_train))
+            loss = (1 / x_train.shape[0] *
+                    reduce(add, list(map(lambda x, y: (x - y) ** 2, y_pred, y_train))))
+            grad_mse = 2 / x_train.shape[0] * np.dot(residuals, x_train)               # градиент MSE
             self.weights = self.weights - self.learning_rate * grad_mse    # шаг в сторону антиградиента
 
             # вывод лога обучения модели
@@ -39,7 +47,24 @@ class MyLineReg:
                     print(f'{(i + 1) // verbose * verbose} | loss: {loss}')
 
     def get_coef(self):
+        """
+        Функция возвращает веса обученной модели.
+
+        :return: Список весов
+        """
         return self.weights[1:]
+
+    def predict(self, x_test):
+        """
+        Функция делает предсказание на новых данных
+
+        :param x_test: Тестовая выборка
+        :return:
+        """
+        x_test.insert(0, 'col_ones', 1)
+        x_test = x_test.to_numpy()
+        y_pred = np.dot(x_test, self.weights)
+        return y_pred
 
     def __str__(self):
         return f'MyLineReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}'
--- Linear Regression/testing.py
+++ Linear Regression/testing.py
@@ -3,18 +3,31 @@ import pandas as pd
 import numpy as np
 from main import MyLineReg
 
-X, y = make_regression(n_samples=1000,
-                       n_features=5,
-                       n_informative=2,
-                       noise=15,
-                       random_state=42)
-X = pd.DataFrame(X)
-y = pd.Series(y)
-X.columns = [f'col_{col}' for col in X.columns]
+x_train, y_train = make_regression(n_samples=1000,
+                                   n_features=5,
+                                   n_informative=2,
+                                   noise=15,
+                                   random_state=42)
+x_train = pd.DataFrame(x_train)
+y_train = pd.Series(y_train)
+x_train.columns = [f'col_{col}' for col in x_train.columns]
+
+
+x_test, y_test = make_regression(n_samples=5,
+                                   n_features=5,
+                                   n_informative=2,
+                                   noise=15,
+                                   random_state=42)
+x_test = pd.DataFrame(x_test)
+y_test = pd.Series(y_test)
+x_test.columns = [f'col_{col}' for col in x_test.columns]
+
 
 a = MyLineReg()
-a.fit(X, y, verbose=False)
-print(a.get_coef())
+a.fit(x_train, y_train, verbose=False)
+print(f'Веса обученной модели: {a.get_coef()}')
+print(f'Предсказания: {a.predict(x_test)}')
+print(x_test)
 
 
 
